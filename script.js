9da188b5e6e808b2964e5de13021a585fa9d8045
dinadahliana <dina.dahliana@students.itb.ac.id>
2023-03-09 15:47:33 +0700

    add mathjs and summary event listener

--- script.js
+++ script.js
@@ -1,10 +1,42 @@
 const toolsButton = document.querySelectorAll(".button");
-const display = document.querySelector(".display");
+const display = document.querySelector(".input");
+const summary = document.querySelector(".equal");
+const reset = document.querySelector(".clear");
+let output = document.querySelector(".summary");
+
+const operator = ["+", "-", "*", "/"];
+const other = ["=", "C", "+/-"];
+let input = "";
 
 toolsButton.forEach(btn => {
     btn.addEventListener("click", (e) => {
-        display.innerText = e.target.innerText;
+        const buttonContent = e.target.innerText
+        if (operator.includes(buttonContent)) {
+            input += ` ${buttonContent} `;
+            display.innerText = input;
+        } else if (other.includes(buttonContent)) {
+            display.innerText = "";
+        } else {
+            input += `${buttonContent}`;
+            display.innerText = input;
+        }     
         console.log(`click: ${e.target.innerHTML}`);
     })
-})
+    
+});
 
+summary.addEventListener("click", () =>{
+    console.log(input);
+    
+    console.log(math.evaluate(input));
+    
+    output.innerHTML = math.evaluate(input);
+    
+    input = "";   
+});
+
+reset.addEventListener("click", () => {
+    input = "";
+    input.innerHTML = "";
+    output.innerHTML = "";
+})
